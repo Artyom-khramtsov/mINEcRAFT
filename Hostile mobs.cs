f51237c19ca3543e22d7dc4143c7df6e334e3fc2
Artyom-khramtsov <71020590+Artyom-khramtsov@users.noreply.github.com>
2021-03-06 16:59:29 +0300

    дружелюбные мобы. И лайк,дизлайк
    забыл в прошлый раз сохранится

---
+++ Hostile mobs.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Проверка
+{
+    public struct Searchmobs
+    {
+        public PictureBox picture;
+        public Label label;
+        public string name;
+        public string land;
+        public string category;
+
+        public Searchmobs(string name1, string land1, string Category)
+        {
+            name = name1;
+            land = land1;
+            label = new Label();
+            picture = new PictureBox();
+            category = Category;
+        }
+    }
+
+
+    public partial class Mobs : Form
+    {
+        /// <summary>
+        /// Все мобы
+        /// </summary>
+        public static List<Searchmobs> mob_list = new List<Searchmobs>();
+
+        public static List<Searchmobs> Mob_like = new List<Searchmobs>();
+
+        public static List<Searchmobs> Mob_dislike = new List<Searchmobs>();
+
+        public Mobs()
+        {
+            InitializeComponent();
+
+            mob_list.Add(new Searchmobs("Ифрит", "Ад", "Враждебные_мобы"));
+            mob_list.Add(new Searchmobs("Зомби-наездник", "Верхний мир", "Враждебные_мобы"));
+            mob_list.Add(new Searchmobs("Крипер", "Верхний мир", "Враждебные_мобы"));
+            mob_list.Add(new Searchmobs("Утопленник", "Верхний мир", "Враждебные_мобы"));
+            mob_list.Add(new Searchmobs("Древний страж", "Верхний мир", "Враждебные_мобы"));
+            mob_list.Add(new Searchmobs("Чешуйница Края", "Верхний мир, Ад, Край", "Враждебные_мобы"));
+            mob_list.Add(new Searchmobs("Вызыватель", "Верхний мир", "Враждебные_мобы"));
+            mob_list.Add(new Searchmobs("Гаст", "Ад", "Враждебные_мобы"));
+            mob_list.Add(new Searchmobs("Страж", "Верхний мир", "Враждебные_мобы"));
+            mob_list.Add(new Searchmobs("Хоглин", "Ад", "Враждебные_мобы"));
+            mob_list.Add(new Searchmobs("Кадавр", "Верхний мир", "Враждебные_мобы"));
+            mob_list.Add(new Searchmobs("Лавовый куб", "Ад", "Враждебные_мобы"));
+            mob_list.Add(new Searchmobs("Фантом", "Верхний мир", "Враждебные_мобы"));
+            mob_list.Add(new Searchmobs("Жестокий пинглин", "Ад", "Враждебные_мобы"));
+            mob_list.Add(new Searchmobs("Разбойник", "Верхний мир", "Враждебные_мобы"));
+            mob_list.Add(new Searchmobs("Разоритель", "Верхний мир", "Враждебные_мобы"));
+            mob_list.Add(new Searchmobs("Шалкер", "Край", "Враждебные_мобы"));
+            mob_list.Add(new Searchmobs("Чешуйница", "Верхний мир", "Враждебные_мобы"));
+            mob_list.Add(new Searchmobs("Скелет", "Верхний мир", "Враждебные_мобы"));
+            mob_list.Add(new Searchmobs("Cкелет-наездник", "Верхний мир", "Враждебные_мобы"));
+            mob_list.Add(new Searchmobs("Слизень", "Верхний мир", "Враждебные_мобы"));
+            mob_list.Add(new Searchmobs("Зимогор", "Верхний мир", "Враждебные_мобы"));
+            mob_list.Add(new Searchmobs("Досаждатель", "Верхний мир", "Враждебные_мобы"));
+            mob_list.Add(new Searchmobs("Поборник", "Верхний мир", "Враждебные_мобы"));
+            mob_list.Add(new Searchmobs("Ведьма", "Верхний мир", "Враждебные_мобы"));
+            mob_list.Add(new Searchmobs("Скелет-иссушитель", "Ад", "Враждебные_мобы"));
+            mob_list.Add(new Searchmobs("Зоглин", "Ад", "Враждебные_мобы"));
+            mob_list.Add(new Searchmobs("Зомби", "Верхний мир", "Враждебные_мобы"));
+            mob_list.Add(new Searchmobs("Зомби-житель", "Верхний мир", "Враждебные_мобы"));
+
+
+            int x = 10;
+            int y = 80;
+            for (int i = 0; i < mob_list.Count; i++)
+            {
+
+                mob_list[i].picture.BackColor = Color.Transparent;
+                mob_list[i].picture.Tag = mob_list[i].name;
+                mob_list[i].picture.Location = new Point(x, y);
+                mob_list[i].picture.Size = new Size(300, 300);
+                mob_list[i].picture.SizeMode = PictureBoxSizeMode.Zoom;
+                mob_list[i].picture.Click += new EventHandler(OpenMob);
+                try
+                {
+                    mob_list[i].picture.Load("../../Враждебные_мобы/" + mob_list[i].name + ".gif");
+                }
+                catch (Exception)
+                {
+                    mob_list[i].picture.Load("../../Враждебные_мобы/" + mob_list[i].name + ".png");
+                }
+                Controls.Add(mob_list[i].picture);
+
+
+                mob_list[i].label.AutoSize = true;
+                mob_list[i].label.BackColor = Color.Transparent;
+                mob_list[i].label.ForeColor = Color.Red;
+                mob_list[i].label.Font = new Font("Microsoft Sans Serif", 15F);
+                mob_list[i].label.Location = new Point(x + 100, y + 300);
+                mob_list[i].label.Size = new Size(300, 29);
+                mob_list[i].label.TabIndex = 31;
+                mob_list[i].label.Text = mob_list[i].name;
+                Controls.Add(mob_list[i].label);
+
+
+                x = x + 300;
+                if (x + 300 > Width)
+                {
+                    x = 10;
+                    y = y + 350;
+                }
+            }
+        }
+       
+
+        private void OpenMob(object sender, EventArgs e)
+        {
+            PictureBox picture = (PictureBox)sender;
+            MobForm form = new MobForm(picture.Tag.ToString());
+            form.Show();
+             
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int x = 10;
+            int y = 80;
+            for (int i = 0; i < mob_list.Count; i++)
+            {
+                mob_list[i].picture.Visible = true;
+                mob_list[i].label.Visible = true;
+                if (textBox1.Text != "" &&
+                    !mob_list[i].name.Contains(textBox1.Text))
+                {
+                    mob_list[i].picture.Visible = false;
+                    mob_list[i].label.Visible = false;
+                }
+
+                if (comboBox1.Text != "" &&
+                    !mob_list[i].land.Contains(comboBox1.Text))
+                    //mobs[i].land != comboBox1.Text)
+                {
+                    mob_list[i].picture.Visible = false;
+                    mob_list[i].label.Visible = false;
+                }
+
+
+                if (mob_list[i].picture.Visible)
+                {
+                    mob_list[i].picture.Location = new Point(x, y);
+                    mob_list[i].label.Location = new Point(x + 100, y + 300);
+
+                    x = x + 300;
+                    if (x + 300 > Width)
+                    {
+                        x = 10;
+                        y = y + 350;
+                    }
+                }
+            }
+
+
+
+        }
+      
+    }
+}
---
+++ MobForm.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Проверка
+{
+   
+
+    public partial class MobForm : Form
+    {
+        string Mob_Like;
+
+        public MobForm(string MobType)
+        {
+            InitializeComponent();
+
+            Mob_Like = MobType;
+            Text = MobType;
+            label2.Text = MobType;
+
+            string category = "Враждебные мобы";
+            for (int i = 0; i < Mobs.mob_list.Count; i++)
+            {
+                if (MobType == Mobs.mob_list[i].name)
+                {
+                    category = Mobs.mob_list[i].category;
+                }
+            }
+            try
+            {
+                label3.Text = File.ReadAllText("../../" + category + "/" + MobType + ".txt");
+                label1.Text = File.ReadAllText("../../" + category + "/" + MobType + "2.txt");
+            }
+            catch (Exception) { }
+
+            try
+            {
+                pictureBox1.Load("../../" + category + "/" + MobType + ".gif");
+            }
+            catch (Exception)
+            {
+                pictureBox1.Load("../../" + category + "/" + MobType + ".png");
+            }
+
+            
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            //Бегаем по всем мобам
+            foreach (Searchmobs mob in Mobs.mob_list)
+            { 
+                //Нашли нужного
+                if(mob.name == Mob_Like)
+                {
+                    //Добавляем в список лайкнутых
+                    Mobs.Mob_like.Add(mob);
+
+                }
+            }
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            //Бегаем по всем мобам
+            foreach (Searchmobs mob in Mobs.Mob_dislike)
+            {
+                //Нашли нужного
+                if (mob.name == Mob_Like)
+                {
+                    //Добавляем в список лайкнутых
+                    Mobs.Mob_dislike.Add(mob);
+
+                }
+            }
+        }
+    }
+}
